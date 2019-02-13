07f6c6fc01cd7baf9d9be2c3f835d70d8c8fbffd
southwind9801 <ningnan9801@163.com>
2019-02-13 17:42:21 +0800

    第3课，服务消费者

---
+++ consumer/src/main/java/com/southwind/ConsumerApplication.java
@@ -0,0 +1,18 @@
+package com.southwind;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.client.RestTemplate;
+
+@SpringBootApplication
+public class ConsumerApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(ConsumerApplication.class,args);
+    }
+
+    @Bean
+    public RestTemplate restTemplate(){
+        return new RestTemplate();
+    }
+}
---
+++ consumer/src/main/java/com/southwind/controller/StudentHandler.java
@@ -0,0 +1,41 @@
+package com.southwind.controller;
+
+import com.southwind.entity.Student;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.client.RestTemplate;
+
+import java.util.Collection;
+
+@RequestMapping("/consumer")
+@RestController
+public class StudentHandler {
+
+    @Autowired
+    private RestTemplate restTemplate;
+
+    @GetMapping("/findAll")
+    public Collection<Student> findAll(){
+        return restTemplate.getForObject("http://localhost:8010/student/findAll",Collection.class);
+    }
+
+    @GetMapping("/findById/{id}")
+    public Student findById(@PathVariable("id") long id){
+        return restTemplate.getForObject("http://localhost:8010/student/findById/{id}",Student.class,id);
+    }
+
+    @PostMapping("/save")
+    public void save(@RequestBody Student student){
+        restTemplate.postForObject("http://localhost:8010/student/save",student,Student.class);
+    }
+
+    @PutMapping("/update")
+    public void update(@RequestBody Student student){
+        restTemplate.put("http://localhost:8010/student/update",student);
+    }
+
+    @DeleteMapping("/deleteById/{id}")
+    public void deleteById(@PathVariable("id") long id){
+        restTemplate.delete("http://localhost:8010/student/deleteById/{id}",id);
+    }
+}
