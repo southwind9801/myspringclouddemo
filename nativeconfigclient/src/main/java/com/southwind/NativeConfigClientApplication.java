368e4d70b0aeefe4b5f6c2b3291da51b534f30a8
southwind9801 <ningnan9801@163.com>
2019-02-14 17:53:58 +0800

    第8课，配置中心

---
+++ nativeconfigclient/src/main/java/com/southwind/NativeConfigClientApplication.java
@@ -0,0 +1,11 @@
+package com.southwind;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NativeConfigClientApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(NativeConfigClientApplication.class,args);
+    }
+}
